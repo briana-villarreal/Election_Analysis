c0deaa7c9cc242cecc16b7aac3d97dda218debe1
briana-villarreal <bcelestev@gmail.com>
2022-06-26 12:49:54 -0500

    Adding the election analysis.

--- Python_practice.py
+++ Python_practice.py
@@ -1,4 +1,6 @@
+import csv
+import os
 print("Hello World")
-countries=["Arapahoe", "Denver","Jefferson"]
-if countries [1]=="Denver":
+countries = ["Arapahoe", "Denver", "Jefferson"]
+if countries[1] == "Denver":
     print(countries[1])
